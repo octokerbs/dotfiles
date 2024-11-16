13b0189cea1ea4d51285cbe444031aef2d0111ee
okerbs <okerbs@proton.me>
2024-11-16 13:02:16 -0300

    Back to material

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.shiftwidth = 4 -- Size of an indent
-vim.opt.tabstop = 4 -- Number of spaces tabs count for
+-- Tabs
+vim.opt.shiftwidth = 8 -- Size of an indent
+vim.opt.tabstop = 8 -- Number of spaces tabs count for
 vim.opt.expandtab = true -- Use spaces instead of tabs
+
+-- Text
+vim.opt.wrap = true
+vim.opt.textwidth = 80
